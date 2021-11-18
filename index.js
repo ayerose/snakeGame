087cbff0864d2052e8ac26dc02498b87bb306b30
ayerose <reya-k@outlook.de>
2021-11-18 18:10:51 +0000

    update

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 const grid = document.querySelector('.grid');
 const startButton = document.getElementById('start');
 const score = document.getElementById('score');
-let squares = []
-let currentSnake = [2,1,0]
+let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let width = 10;
 
 function createGrid() {
   //create 100 of these elements with a for loop
   for (let i = 0; i < 100; i++) {
     //create element
     const square = document.createElement('div');
-    console.log(square);
     //add styling to the element
     square.classList.add('square');
     //put the element into our grid
     grid.appendChild(square);
     //push it into a new squares array
-    squares.push(square)
+    squares.push(square);
   }
 }
 createGrid();
 
-currentSnake.forEach(index => squares[index].classList.add('snake'))
+currentSnake.forEach((index) => squares[index].classList.add('snake'));
 
 function move() {
   //remove last element from our currentSnake array
-  const tail = currentSnake.pop()
+  const tail = currentSnake.pop();
+
   //remove styling from last element
-  squares[tail].classList.remove('snake')
-
-  //add sqaure in direction we are heading
-
+  squares[tail].classList.remove('snake');
+  //add square in direction we are heading
+  currentSnake.unshift(currentSnake[0] + direction);
+ 
   //add styling so we can see it
+  squares[currentSnake[0]].classList.add('snake')
 }
+move();
+
+setInterval(move, 1000)
+function control(e) {
+  if (e.keyCode === 39) {
+    console.log('right pressed');
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    console.log('up pressed');
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    console.log('left pressed');
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    console.log('down pressed');
+    direction = +width;
+  }
+}
+document.addEventListener('keyup', control);
